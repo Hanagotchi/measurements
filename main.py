42fd238ba0bf4bba2362eea909e3f39697cbc944
Agustinefe <agustinefe@hotmail.com>
2024-01-01 18:42:48 -0300

    Add documentation

--- main.py
+++
@@ -1,7 +0,0 @@
-from database.mongo import Mongo
-from database.postgres import Postgres
-
-from service_tests.database.test import test_DB
-
-
-test_DB(Postgres())
--- service_tests/rabbitmq/sender.py
+++ service_tests/rabbitmq/sender.py
@@ -1,13 +1,14 @@
 import pika, time
+from pika.exchange_type import ExchangeType
 
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='fiufit-metrics-queue')
 
 for i in range(0, 100):
-    channel.basic_publish(exchange='', routing_key='hello', body=str(i))
+    channel.basic_publish(exchange='fiutfit-metrics-exchange', routing_key='fiufit-metrics', body=str(i))
     print(f" [x] Sent '{i}'")
     time.sleep(1)
 
