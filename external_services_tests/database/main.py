76a5c1783ae98a94e4cadacfbb731e9221234e65
Agustinefe <agustinefe@hotmail.com>
2024-01-02 00:47:09 -0300

    Create test for Measurements table

--- external_services_tests/database/main.py
+++ external_services_tests/database/main.py
@@ -1,4 +1,5 @@
-from tests import test_DB
+from tests import test_DB_device_plant, test_DB_measurements
 from sql_alchemy import SQL_Alchemy
 
-test_DB(SQL_Alchemy())
+#test_DB_device_plant(SQL_Alchemy())
+test_DB_measurements(SQL_Alchemy())
--- external_services_tests/database/sql_alchemy.py
+++ external_services_tests/database/sql_alchemy.py
@@ -1,8 +1,9 @@
-from sqlalchemy import CheckConstraint, create_engine, Integer, String, SmallInteger, select, delete
+from sqlalchemy import CheckConstraint, create_engine, Integer, String, SmallInteger, select, delete, update
 from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase, Session
 from database import Database
 from dotenv import load_dotenv
 from os import environ
+from typing import Optional, Union
 
 load_dotenv()
 
@@ -29,11 +30,11 @@ class Measurement(Base):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     id_plant: Mapped[int] = mapped_column(Integer, unique=True)
     plant_type: Mapped[int] = mapped_column(SmallInteger)
-    timestamp: Mapped[str] = mapped_column(String(50))
-    temperature: Mapped[int] = mapped_column(SmallInteger)
-    humidity: Mapped[int] = mapped_column(SmallInteger)
-    light: Mapped[int] = mapped_column(SmallInteger)
-    watering: Mapped[int] = mapped_column(SmallInteger)
+    time_stamp: Mapped[str] = mapped_column(String(50))
+    temperature: Mapped[Optional[int]] = mapped_column(SmallInteger)
+    humidity: Mapped[Optional[int]] = mapped_column(SmallInteger)
+    light: Mapped[Optional[int]] = mapped_column(SmallInteger)
+    watering: Mapped[Optional[int]] = mapped_column(SmallInteger)
 
     __table_args__ = (
         CheckConstraint('humidity >= 0 AND humidity <= 100', name='check_humidity'),
@@ -43,13 +44,12 @@ class Measurement(Base):
     )
 
     def __repr__(self):
-        return f"Measurement(id={self.id!r}, id_plant={self.id_plant!r}, plant_type={self.plant_type!r}, timestamp={self.timestamp!r}, temperature={self.temperature!r}, humidity={self.humidity!r}, light={self.light!r}, watering={self.watering!r})"
+        return f"Measurement(id={self.id!r}, id_plant={self.id_plant!r}, plant_type={self.plant_type!r}, time_stamp={self.time_stamp!r}, temperature={self.temperature!r}, humidity={self.humidity!r}, light={self.light!r}, watering={self.watering!r})"
 
 
-class SQL_Alchemy(Database):
+class SQL_Alchemy():
 
     def __init__(self):
-
         db_url = f'postgresql://{environ["POSTGRES_USER"]}:{environ["POSTGRES_PASSWORD"]}@{environ["POSTGRES_HOST"]}:{environ["POSTGRES_PORT"]}/{environ["POSTGRES_DB"]}'
         self.engine = create_engine(db_url)
         self.conn = self.engine.connect()
@@ -58,20 +58,21 @@ class SQL_Alchemy(Database):
     def shutdown(self):
         self.conn.close()
 
-    def insert_into_device_plant(self, id_device: str, id_plant: int, plant_type: int, id_user: int):
-        
-        device_plant = DevicePlant(
-            id_device=id_device,
-            id_plant=id_plant,
-            plant_type=plant_type,
-            id_user=id_user,
-        )
-
+    def clean_table(self, table: Union[DevicePlant, Measurement]):
         with Session(self.engine) as session:
-            session.add(device_plant)
+            query = delete(table)
+            session.execute(query)
             session.commit()
 
-    def find_in_device_plant(self, id_device: str) -> DevicePlant:
+    def add_new(self, record: Union[DevicePlant, Measurement]):
+        with Session(self.engine) as session:
+            session.add(record)
+            session.commit()
+
+
+    ### DEVICE_PLANT TABLE ###
+    
+    def find_device_plant(self, id_device: str) -> DevicePlant:
 
         query = select(DevicePlant).where(DevicePlant.id_device == id_device)
 
@@ -80,8 +81,22 @@ class SQL_Alchemy(Database):
         
         return result
     
-    def clean_device_plant(self):
+    def update_device_plant(self, id_device: str, id_plant: Optional[int], plant_type: Optional[int], id_user: Optional[int]):
+        query = select(DevicePlant).where(DevicePlant.id_device == id_device)
+
         with Session(self.engine) as session:
-            query = delete(DevicePlant)
-            session.execute(query)
+            device_plant = session.scalars(query).one()
+            if id_plant: device_plant.id_plant = id_plant
+            if plant_type: device_plant.id_plant = plant_type
+            if id_user: device_plant.id_user = id_user
             session.commit()
+
+    ### MEASUREMENTS TABLE ###
+
+    def get_last_measurement(self, id_plant: int) -> Measurement:
+        query = select(Measurement).where(Measurement.id_plant == id_plant).order_by(Measurement.id.desc()).limit(1)
+
+        with Session(self.engine) as session:
+            result: Measurement = session.scalars(query).one()
+        
+        return result
