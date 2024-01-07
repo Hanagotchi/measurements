3f9bbf182764d570dd16dfd9b29320645a12e9eb
Agustinefe <agustinefe@hotmail.com>
2024-01-07 17:55:25 -0300

    Add logger

--- app/main.py
+++ app/main.py
@@ -2,25 +2,36 @@ from fastapi import Body, FastAPI, Request
 from app.database.database import SQLAlchemyClient
 from app.database.models.DevicePlant import DevicePlant
 from app.schemas.DevicePlant import DevicePlantSchema
-
+import logging
 
 app = FastAPI()
+logger = logging.getLogger("measurements")
+logger.setLevel("DEBUG")
 
 @app.on_event("startup")
 async def start_up():
-    app.database = SQLAlchemyClient()
+    app.logger = logger
+
+    try:
+        app.database = SQLAlchemyClient()
+        app.logger.info("Postgres connection established")
+    except Exception as e:
+        app.logger.error(e)
+        app.logger.error("Could not connect to Postgres client")
 
 
 @app.on_event("shutdown")
 async def shutdown_db_client():
     app.database.shutdown()
-
+    app.logger.info("Postgres shutdown succesfully")
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+
+# Endpoint only for DB conection testing.
 @app.post("/device-plant")
 async def add_new_device_plant(req: Request, device_plant: DevicePlantSchema = Body(...)):
     req.app.database.add_new(DevicePlant.from_pydantic(device_plant))
