cedd06645b51863f55b84e0303f6beacb5c409aa
violeta <viperez@fi.uba.ar>
2024-01-08 21:57:27 -0300

    refactor+add healthcheck and logging

--- app/main_rabbitmq.py
+++ app/main_rabbitmq.py
@@ -1,47 +1,28 @@
-import time
-from service.common.middleware import Middleware
-from sqlalchemy import Column, Integer, String, MetaData
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 import os
-import json
-
-# TODO: agregar healthcheks en el docker compose
-time.sleep(17)
-middleware = Middleware()
-Base = declarative_base(metadata=MetaData(schema='dev'))
-
-
-class DevicePlant(Base):
-    __tablename__ = "device_plant"
-    id_device = Column(String(32), primary_key=True)
-    id_plant = Column(Integer, nullable=False, unique=True)
-    plant_type = Column(Integer, nullable=False)
-    id_user = Column(Integer, nullable=False)
-
-
-dbUrl = os.environ.get("DATABASE_URL")
-engine = create_engine(dbUrl, echo=True, future=True)
-session = Session(engine)
-
-
-def callback(body):
-    print("Entro al callback")
-    body = json.loads(body)
-    print(body)
-    dp = DevicePlant(id_device=body["id_device"],
-                     id_plant=body["id_plant"],
-                     plant_type=body["plant_type"],
-                     id_user=body["id_user"])
-    session.add(dp)
-    session.commit()
-    print("Se agrego a la base de datos")
+import logging
+from service.rabbitmq.consumer import Consumer
 
 
 def main():
-    middleware.create_queue("device_plant")
-    middleware.listen_on("device_plant", callback)
+    logging_level = os.environ.get("LOGGING_LEVEL")
+    queue_name = os.environ.get("QUEUE_NAME")
+    initialize_log(logging_level)
+    consumer = Consumer(queue_name)
+    consumer.run()
+
+
+def initialize_log(logging_level):
+    """
+    Python custom logging initialization
+
+    Current timestamp is added to be able to identify in docker
+    compose logs the date when the log has arrived
+    """
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        level=logging_level,
+        datefmt='%Y-%m-%d %H:%M:%S',
+    )
 
 
 if __name__ == "__main__":
