4a117b9dcf9bd905f3732cf2760dfcad12a3989a
Agustinefe <agustinefe@hotmail.com>
2024-01-17 20:54:24 -0300

    Add handler for UniqueViolation error

--- app/controller/device_plant_controller.py
+++ app/controller/device_plant_controller.py
@@ -1,4 +1,4 @@
-from fastapi import Request, Response, status, HTTPException
+from fastapi import Request, status, HTTPException
 from database.models.device_plant import DevicePlant
 from schemas.device_plant import DevicePlantSchema
 import logging
@@ -15,9 +15,19 @@ def create_device_plant_relation(req: Request, device_plant: DevicePlantSchema):
         return req.app.database.find_device_plant(device_plant.id_device)
     except IntegrityError as err:
         req.app.database.rollback()
-        logger.warning(err)
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=format(err))
+
+        if isinstance(err.orig, UniqueViolation):
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=err.orig)
+
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=err)
     except PendingRollbackError as err:
         req.app.database.rollback()
         logger.warning(format(err))
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=format(err))
+
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=err)
