ea1a7e571d441bf7e5f99dca856bb4adfc7cb9c5
Agustinefe <agustinefe@hotmail.com>
2024-01-21 13:17:53 -0300

    Create decorator for error handling

--- app/controller/device_plant_controller.py
+++ app/controller/device_plant_controller.py
@@ -15,64 +15,62 @@ logger = logging.getLogger("app")
 logger.setLevel("DEBUG")
 
 
-def withSQLExceptionsHandle(callback):
-    try:
-        return callback()
-    except IntegrityError as err:
-        if isinstance(err.orig, UniqueViolation):
-            parsed_error = err.orig.pgerror.split("\n")
+def withSQLExceptionsHandle(func):
+
+    def handleSQLException(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except IntegrityError as err:
+            if isinstance(err.orig, UniqueViolation):
+                parsed_error = err.orig.pgerror.split("\n")
+                raise HTTPException(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    detail={
+                        "error": parsed_error[0],
+                        "detail": parsed_error[1]
+                    })
+
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=format(err))
+
+        except PendingRollbackError as err:
+            logger.warning(format(err))
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=format(err))
+
+        except NoResultFound as err:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
-                detail={
-                    "error": parsed_error[0],
-                    "detail": parsed_error[1]
-                })
+                detail=format(err))
 
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=format(err))
-
-    except PendingRollbackError as err:
-        logger.warning(format(err))
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=format(err))
-
-    except NoResultFound as err:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail=format(err))
+    return handleSQLException
 
 
+@withSQLExceptionsHandle
 def create_device_plant_relation(req: Request, device_plant: DevicePlantSchema):
-
-    def create_new_relation():
-        try:
-            req.app.database.add_new(DevicePlant.from_pydantic(device_plant))
-            return req.app.database.find_device_plant(device_plant.id_device)
-        except Exception as err:
-            req.app.database.rollback()
-            raise err
-
-    withSQLExceptionsHandle(create_new_relation)
+    try:
+        req.app.database.add_new(DevicePlant.from_pydantic(device_plant))
+        return req.app.database.find_device_plant(device_plant.id_device)
+    except Exception as err:
+        req.app.database.rollback()
+        raise err
 
 
+@withSQLExceptionsHandle
 def update_device_plant(req: Request,
                         id_device: str,
                         device_plant_update_set:
                         Union[DevicePlantUpdateSchema, DevicePlantPartialUpdateSchema]):
-
-    def create_new_relation():
-        try:
-            req.app.database.update_device_plant(
-                id_device,
-                device_plant_update_set.id_plant,
-                device_plant_update_set.plant_type,
-                device_plant_update_set.id_user,
-            )
-            return req.app.database.find_device_plant(id_device)
-        except Exception as err:
-            req.app.database.rollback()
-            raise err
-
-    withSQLExceptionsHandle(create_new_relation)
+    try:
+        req.app.database.update_device_plant(
+            id_device,
+            device_plant_update_set.id_plant,
+            device_plant_update_set.plant_type,
+            device_plant_update_set.id_user,
+        )
+        return req.app.database.find_device_plant(id_device)
+    except Exception as err:
+        req.app.database.rollback()
+        raise err
