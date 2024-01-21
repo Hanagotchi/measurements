9c20c45ca9f7d6e56bca5b6156e0e9eb6309e93a
Agustinefe <agustinefe@hotmail.com>
2024-01-21 11:30:25 -0300

    Handle NotResultFound

--- app/controller/device_plant_controller.py
+++ app/controller/device_plant_controller.py
@@ -8,19 +8,17 @@ from schemas.device_plant import (
 )
 import logging
 from psycopg2.errors import UniqueViolation
-from sqlalchemy.exc import PendingRollbackError, IntegrityError
+from sqlalchemy.exc import PendingRollbackError, IntegrityError, NoResultFound
 
 
 logger = logging.getLogger("app")
 logger.setLevel("DEBUG")
 
 
-def create_device_plant_relation(req: Request, device_plant: DevicePlantSchema):
+def withSQLExceptionsHandle(callback):
     try:
-        req.app.database.add_new(DevicePlant.from_pydantic(device_plant))
-        return req.app.database.find_device_plant(device_plant.id_device)
+        return callback()
     except IntegrityError as err:
-        req.app.database.rollback()
 
         if isinstance(err.orig, UniqueViolation):
             parsed_error = err.orig.pgerror.split("\n")
@@ -35,45 +33,46 @@ def create_device_plant_relation(req: Request, device_plant: DevicePlantSchema):
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=format(err))
     except PendingRollbackError as err:
-        req.app.database.rollback()
         logger.warning(format(err))
 
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=format(err))
+    except NoResultFound as err:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=format(err))
+
+
+def create_device_plant_relation(req: Request, device_plant: DevicePlantSchema):
+
+    def create_new_relation():
+        try:
+            req.app.database.add_new(DevicePlant.from_pydantic(device_plant))
+            return req.app.database.find_device_plant(device_plant.id_device)
+        except Exception as err:
+            req.app.database.rollback()
+            raise err
+
+    withSQLExceptionsHandle(create_new_relation)
 
 
 def update_device_plant(req: Request,
                         id_device: str,
                         device_plant_update_set:
                         Union[DevicePlantUpdateSchema, DevicePlantPartialUpdateSchema]):
-    try:
-        req.app.database.update_device_plant(
-            id_device,
-            device_plant_update_set.id_plant,
-            device_plant_update_set.plant_type,
-            device_plant_update_set.id_user,
-        )
-        return req.app.database.find_device_plant(id_device)
-    except IntegrityError as err:
-        req.app.database.rollback()
 
-        if isinstance(err.orig, UniqueViolation):
-            parsed_error = err.orig.pgerror.split("\n")
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail={
-                    "error": parsed_error[0],
-                    "detail": parsed_error[1]
-                })
+    def create_new_relation():
+        try:
+            req.app.database.update_device_plant(
+                id_device,
+                device_plant_update_set.id_plant,
+                device_plant_update_set.plant_type,
+                device_plant_update_set.id_user,
+            )
+            return req.app.database.find_device_plant(id_device)
+        except Exception as err:
+            req.app.database.rollback()
+            raise err
 
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=format(err))
-    except PendingRollbackError as err:
-        req.app.database.rollback()
-        logger.warning(format(err))
-
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=format(err))
+    withSQLExceptionsHandle(create_new_relation)
