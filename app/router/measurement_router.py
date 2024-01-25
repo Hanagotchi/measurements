fcf30a8df09a6ac47b3cdc2f2280a9cd09e3611f
Federico <fpacheco@fi.uba.ar>
2024-01-25 20:57:23 -0300

    Rename router

--- app/router/measurement_router.py
+++ app/router/measurement_router.py
@@ -7,9 +7,9 @@ from fastapi import Request, status, HTTPException
 measurement = APIRouter()
 
 @measurement.get(
-        "/{id_device}/last",
+        "/{id_plant}/last",
         status_code=status.HTTP_200_OK,
         response_model=MeasurementSavedSchema
         )
-async def last_measurement_made_by_device(req: Request, id_device: str):
+async def last_measurement_made_by_device(req: Request, id_plant: int):
     raise HTTPException(status_code=501, detail="Not implemented :c")
