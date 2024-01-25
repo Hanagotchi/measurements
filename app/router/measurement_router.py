78dc6b9a922179c1b4275f6f645214b9a9f6d14e
Federico <fpacheco@fi.uba.ar>
2024-01-25 20:40:05 -0300

    Add router for last measurement

---
+++ app/router/measurement_router.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, Body, Request, status
+from schemas.measurement import (
+    MeasurementSavedSchema
+)
+from fastapi import Request, status, HTTPException
+
+measurement = APIRouter()
+
+@measurement.get(
+        "/{id_device}/last",
+        status_code=status.HTTP_200_OK,
+        response_model=MeasurementSavedSchema
+        )
+async def last_measurement_made_by_device(req: Request, id_device: str):
+    raise HTTPException(status_code=501, detail="Not implemented :c")
