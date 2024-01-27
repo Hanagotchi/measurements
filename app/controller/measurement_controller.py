830ea7b070b217b540cb348bab1519ec624270e6
Federico <fpacheco@fi.uba.ar>
2024-01-27 17:54:59 -0300

    Endpoint finished

---
+++ app/controller/measurement_controller.py
@@ -0,0 +1,12 @@
+from controller.device_plant_controller import withSQLExceptionsHandle
+from fastapi import Request
+
+
+@withSQLExceptionsHandle
+def last_measurement_made_by_plant(req: Request,
+                                    id_plant: int):
+    try:
+        return req.app.database.get_last_measurement(id_plant)
+    except Exception as err:
+        req.app.database.rollback()
+        raise err
--- app/router/measurement_router.py
+++ app/router/measurement_router.py
@@ -1,15 +1,18 @@
-from fastapi import APIRouter, Body, Request, status
+from fastapi import APIRouter, Request, status
 from schemas.measurement import (
     MeasurementSavedSchema
 )
-from fastapi import Request, status, HTTPException
+from fastapi import Request, status
+from controller import measurement_controller as controller
+
 
 measurement = APIRouter()
+
 
 @measurement.get(
         "/{id_plant}/last",
         status_code=status.HTTP_200_OK,
         response_model=MeasurementSavedSchema
         )
-async def last_measurement_made_by_device(req: Request, id_plant: int):
-    raise HTTPException(status_code=501, detail="Not implemented :c")
+async def last_measurement_made_by_plant(id_plant: int, req: Request):
+    return controller.last_measurement_made_by_plant(req, id_plant)
