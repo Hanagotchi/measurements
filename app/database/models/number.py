c33bd30c33bcd2c6cecfd91a48e813d21337f69b
Federico <fpacheco@fi.uba.ar>
2024-03-03 18:24:29 -0300

    Refactor Number model to include schema from environment variable

--- app/database/models/number.py
+++ app/database/models/number.py
@@ -1,7 +1,9 @@
 from sqlalchemy import Column, Integer
 from database.models.base import Base
+from os import environ
 
 
 class Number(Base):
     __tablename__ = 'numbers'
+    __table_args__ = {'schema': environ.get("MEASUREMENTS_SCHEMA", "dev")}
     number = Column(Integer, primary_key=True)
