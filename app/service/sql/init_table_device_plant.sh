4b4ffea9acf2757218c82351356a75c315429a15
Federico <fpacheco@fi.uba.ar>
2024-03-06 19:42:59 -0300

    Update database initialization scripts

--- app/service/sql/init_table_device_plant.sh
+++ app/service/sql/init_table_device_plant.sh
@@ -7,16 +7,16 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$MEASUREMENTS_DB" 
         IF NOT EXISTS dev.device_plant (
             id_device VARCHAR(32) PRIMARY KEY,
             id_plant INT UNIQUE NOT NULL,
-            plant_type SMALLINT NOT NULL,
+            plant_type VARCHAR(70) NOT NULL,
             id_user INT NOT NULL
     );
 
     DO $do$ BEGIN
         IF (SELECT COUNT(*) FROM dev.device_plant) = 0 THEN
             INSERT INTO dev.device_plant (id_device, id_plant, plant_type, id_user) VALUES 
-                ('fd7c7531467748539f99d2bcef076c88', 1, 1, 1),
-                ('fd8c7531467748539f99d2bcef076c88', 2, 2, 2),
-                ('fd9c7531467748539f99d2bcef076c88', 3, 3, 3);
+                ('fd7c7531467748539f99d2bcef076c88', 1, 'Passiflora caerulea', 1),
+                ('fd8c7531467748539f99d2bcef076c88', 2, 'Kalanchoe daigremontiana', 2),
+                ('fd9c7531467748539f99d2bcef076c88', 3, 'Dracaena fragrans', 3);
         END IF;
     END $do$;
 EOSQL
