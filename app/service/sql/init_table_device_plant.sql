4f8e692a1e68d6cc27ac13f53954f427b7b75095
feijooso <sfeijoo@fi.uba.ar>
2024-03-26 23:46:28 -0300

    fix db

--- app/service/sql/init_table_device_plant.sql
+++ app/service/sql/init_table_device_plant.sql
@@ -3,15 +3,15 @@
         IF NOT EXISTS measurements_service.device_plant (
             id_device VARCHAR(32) PRIMARY KEY,
             id_plant INT UNIQUE NOT NULL,
-            plant_type SMALLINT NOT NULL,
+            plant_type VARCHAR(70) NOT NULL,
             id_user INT NOT NULL
     );
 
     DO $do$ BEGIN
         IF (SELECT COUNT(*) FROM measurements_service.device_plant) = 0 THEN
             INSERT INTO measurements_service.device_plant (id_device, id_plant, plant_type, id_user) VALUES 
-                ('fd7c7531467748539f99d2bcef076c88', 1, 1, 1),
-                ('fd8c7531467748539f99d2bcef076c89', 2, 2, 2),
-                ('fd9c7531467748539f99d2bcef076c90', 3, 3, 3);
+            ('fd7c7531467748539f99d2bcef076c88', 1, 'Passiflora caerulea', 1),
+            ('fd8c7531467748539f99d2bcef076c89', 2, 'Kalanchoe daigremontiana', 2),
+            ('fd9c7531467748539f99d2bcef076c90', 3, 'Dracaena fragrans', 3);
         END IF;
     END $do$;
