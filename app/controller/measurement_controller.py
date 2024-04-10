ec374b73318eb3794e08234d991d3cb47c7e0f3a
Agustinefe <agustin.firmapaz@creativecoefficient.net>
2024-04-09 21:38:34 -0300

    return 204 when there is no last measurement

--- app/controller/measurement_controller.py
+++ app/controller/measurement_controller.py
@@ -1,9 +1,11 @@
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 from controller.device_plant_controller import withSQLExceptionsHandle
-from fastapi import Request
+from fastapi import Request, status
 
 from resources.parser import apply_rules
 from schemas.measurement import MeasurementSavedSchema
-
+from sqlalchemy.exc import NoResultFound
 
 @withSQLExceptionsHandle(async_mode=False)
 def last_measurement_made_by_plant(req: Request, id_plant: int):
@@ -16,6 +18,13 @@ def last_measurement_made_by_plant(req: Request, id_plant: int):
             last_measurement.plant_type
         )
         return last_measurement
+
+    except NoResultFound:
+        return JSONResponse(
+            status_code=status.HTTP_204_NO_CONTENT,
+            content=jsonable_encoder([])
+        )
+
     except Exception as err:
         req.app.database.rollback()
         raise err
